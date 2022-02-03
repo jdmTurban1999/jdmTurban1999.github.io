f515f97694bc6e11977f12e06bfdc62b4a4b03ca
jdmTurban1999 <jdm.turban1699gmail.com>
2022-02-03 23:21:53 +0900

    Add some features

--- dhcafe/assets/js/script.js
+++ dhcafe/assets/js/script.js
@@ -1,3 +1,4 @@
+//スムーズスクロール
 $('a[href^="#"]').click(function() {
     var speed = 800;
     var href= $(this).attr("href");
@@ -5,4 +6,27 @@ $('a[href^="#"]').click(function() {
     var position = target.offset().top;
     $('body,html').animate({scrollTop:position}, speed, 'swing');
     return false;
- });
+});
+
+//上へ戻るボタン
+let topBtn = $('#scroll-top');
+topBtn.hide();
+
+// ある程度スクロールされたら、上に戻るボタンを表示する
+$(window).scroll(function(){
+	console.log($(this).scrollTop());
+	console.log($(this).height());
+	
+	if($(this).scrollTop() > 1000) {
+		topBtn.fadeIn(); // フェードインで表示
+	} else {
+		topBtn.fadeOut(); // フェードアウトで非表示
+	}
+});
+
+topBtn.on("click",function(event) {
+    event.preventDefault();  // ページ内リンクの挙動をキャンセル
+    $('body,html').animate({ // スムーズにスクロールする設定
+        scrollTop: 0
+    }, 500);
+});
